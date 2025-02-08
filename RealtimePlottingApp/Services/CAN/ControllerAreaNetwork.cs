fc454ea7f0766c1aba133e87469bd05bf3a7994b
O-Brob <broborn@student.chalmers.se>
2025-02-08 14:51:59 +0100

    Created the interface for CAN-Bus communication, using Events for receiving messages in an observer pattern-styled fashion, in case new modules will be added in the future which is also interested in the incoming data events. Additionally added ControllerAreaNetwork.cs to select a proper interface implementation depending on OS.

--- RealtimePlottingApp/Services/CAN/ControllerAreaNetwork.cs
+++ RealtimePlottingApp/Services/CAN/ControllerAreaNetwork.cs
@@ -1,6 +1,39 @@
-﻿namespace RealtimePlottingApp.Services.CAN;
+﻿using System;
+using System.Runtime.InteropServices;
 
-public class ControllerAreaNetwork
+namespace RealtimePlottingApp.Services.CAN
 {
-    // Determine what CAN object to return based on OS, and return it via interface abstraction.
+    /// <summary>
+    /// Factory class for creating CAN bus instances.
+    /// It automatically selects the appropriate implementation based on the operating system,
+    /// using SocketCAN for Linux to enable broadest support, and PCAN for Windows due to the lack of libraries with cross-platform support.
+    /// </summary>
+    /// <exception cref="PlatformNotSupportedException">Thrown when the operating system is not supported.</exception>
+    public static class ControllerAreaNetwork
+    {
+        /// <summary>
+        /// Create a new CAN bus instance.
+        /// </summary>
+        /// <returns>A new CAN bus instance.</returns>
+        /// <exception cref="PlatformNotSupportedException">Thrown when the operating system is not supported.</exception>
+        public static ICanBus Create()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                // TODO: Might want something else than prints here eventually.
+                Console.WriteLine("Using SocketCAN for Linux.");
+                return new SocketCanBus();
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                // TODO: Might want something else than prints here eventually.
+                Console.WriteLine("Using PCAN for Windows.");
+                return new PeakCanBus();
+            }
+            else
+            {
+                throw new PlatformNotSupportedException("Only Windows and Linux are supported.");
+            }
+        }
+    }
 }
