f8a84370eda9b3d627f356c4344e1a5b84908c2d
O-Brob <broborn@student.chalmers.se>
2025-02-09 18:39:17 +0100

    Made footer into its own XAML UI component, and gave it a viewmodel of its own. Additionally changed ViewModelBase to a ReactiveObject from ReactiveUI rather than the CommunityToolkit's MVVM package.

---
+++ RealtimePlottingApp/ViewModels/FooterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ReactiveUI;
+using System;
+using System.Windows.Input;
+using Avalonia.Threading;
+
+namespace RealtimePlottingApp.ViewModels
+{
+    // Assume that ViewModelBase inherits from ReactiveObject.
+    public class FooterViewModel : ViewModelBase
+    {
+        // The constructor is used to initialize our commands.
+        public FooterViewModel()
+        {
+            // Initialize commands using ReactiveCommand since all of them are Commands.
+            
+        }
+        
+        // ICommand properties for data binding.
+        
+
+        // ==================== METHOD1 ==================== //
+        
+        
+        
+    }
+}
