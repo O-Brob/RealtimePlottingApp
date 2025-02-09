cbb2083e107055fffcb61966ec262bce7d0ba2d0
O-Brob <broborn@student.chalmers.se>
2025-02-09 13:01:55 +0100

    Split the Sidebar of MainWindow.axaml into its own UserControl component, and added a SidebarViewModel.cs file to handle the logic of the Sidebar.

---
+++ RealtimePlottingApp/ViewModels/SidebarViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ReactiveUI;
+using System;
+using System.Windows.Input;
+using Avalonia.Threading;
+
+namespace RealtimePlottingApp.ViewModels
+{
+    // Assume that ViewModelBase inherits from ReactiveObject.
+    public class SidebarViewModel : ViewModelBase
+    {
+        // The constructor is used to initialize our commands.
+        public SidebarViewModel()
+        {
+            // Initialize commands using ReactiveCommand since all of them are Commands.
+            
+        }
+        
+        // ICommand properties for data binding.
+        
+
+        // ==================== METHOD1 ==================== //
+        
+        
+        
+    }
+}
